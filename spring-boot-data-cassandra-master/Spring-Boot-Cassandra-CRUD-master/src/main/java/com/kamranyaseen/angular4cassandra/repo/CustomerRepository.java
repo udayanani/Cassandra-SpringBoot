490e0cdbd3c63d259c4e39411c63a39257a505b4
udayanani <43642877+udayanani@users.noreply.github.com>
2020-04-27 19:55:53 +0530

    Delete CustomerRepository.java

--- spring-boot-data-cassandra-master/Spring-Boot-Cassandra-CRUD-master/src/main/java/com/kamranyaseen/angular4cassandra/repo/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kamranyaseen.angular4cassandra.repo;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-import com.kamranyaseen.angular4cassandra.model.Customer;
-
-public interface CustomerRepository extends CassandraRepository<Customer> {
-
-}
